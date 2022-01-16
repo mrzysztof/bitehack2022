53c118a6b436a102648a1567bd36236f53b2e3d3
mrzysztof <krzysztof.maliszewski9@wp.pl>
2022-01-16 11:15:10 +0100

    fronstop

--- substitute.js
+++ substitute.js
@@ -1,31 +1,46 @@
-var commentsRoot = document.querySelector("ytd-comments");
-var comments = [];
+const commentsRoot = document.querySelector("ytd-comments");
+var commentElements = [];
+var lastSize = 0;
+commentsRoot.querySelectorAll("ytd-comment-thread-renderer")
+.forEach(node => commentElements.push(node));
+commentElements = commentElements.map(node => node.querySelector("#content-text"));
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 function sendRequest(request){
     let xhr = new XMLHttpRequest();
-    let url = "192.168.3.204:8800";
+    let url = "http://127.0.0.1:8800";
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
         if(xhr.readyState === 4 && xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
-            console.log(response);
+            console.log(response);      
         }
     };
     xhr.send(request);
 }
 
-commentsRoot.querySelectorAll("ytd-comment-thread-renderer")
-.forEach(node => comments.push(node));
-
-comments = comments.map(node => node.querySelector("#content-text"));
-let rawComments = comments.map(c => c.textContent);
-let request = {"comments" : rawComments,
-               "options" : {"maxOffensivity" : "neutral"}};
-
-function update(){
-  let currentComments = document.querySelectorAll("ytd-comments");
-  //if()
+function extractRawComments(directCommentElements){
+    return directCommentElements.map(c => c.textContent);
 }
 
-console.log("end");
+let rawComments = extractRawComments(commentElements);
+let request = {"comments" : rawComments,
+               "options" : {"maxOffensivity" : "neutral"}};
+  
+function update(){
+  let currentCommentElements = commentsRoot.querySelectorAll("ytd-comment-thread-renderer");
+
+  if(currentCommentElements.length >= lastSize){
+    let currentCommentElementsArr = [];
+    let newCommentElements = [];
+    currentCommentElements.forEach(ce => currentCommentElementsArr.push(ce));
+    currentCommentElementsArr = currentCommentElements.map(node => node.querySelector("#content-text"));
+
+    for(let i = lastSize; i < currentCommentElementsArr.length; i++){
+        newCommentElements.push(currentCommentElements.childNodes[i]);
+    }
+     
+    request(extractRawComments(newCommentElements));
+  }
+}
